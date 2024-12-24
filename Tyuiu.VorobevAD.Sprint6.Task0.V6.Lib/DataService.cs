e37759332fda5fc603baef22ae0dc48bf18eeec8
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-24 18:01:45 +0500

    Add project files.

---
+++ Tyuiu.VorobevAD.Sprint6.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.VorobevAD.Sprint6.Task0.V6.Lib
+{
+    public class DataService : tyuiu.cources.programming.interfaces.Sprint6.ISprint6Task0V6
+    {
+        public double Calculate(int x)
+        {
+            return Math.Round(x / Math.Sqrt(Math.Pow(x, 2) + x), 3);
+            throw new NotImplementedException();
+        }
+    }
+}
