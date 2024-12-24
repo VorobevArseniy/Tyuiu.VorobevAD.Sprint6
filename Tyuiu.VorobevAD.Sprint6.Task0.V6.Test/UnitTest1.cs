5141e514a6c628552e2797a11927a2d8f01457b9
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-24 18:03:31 +0500

    GOIDA1

---
+++ Tyuiu.VorobevAD.Sprint6.Task0.V6.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.VorobevAD.Sprint6.Task0.V6.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
