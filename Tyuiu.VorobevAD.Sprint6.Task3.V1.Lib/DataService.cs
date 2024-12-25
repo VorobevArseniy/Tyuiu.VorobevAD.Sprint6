0696515a7e3b314bcdad85dabf15f5a59c735c6f
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-25 23:41:20 +0500

    GOIDA3

---
+++ Tyuiu.VorobevAD.Sprint6.Task3.V1.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.VorobevAD.Sprint6.Task3.V1.Lib;
+
+public class DataService : ISprint6Task3V1
+{
+	public int[,] Calculate(int[,] matrix)
+	{
+		int[,] newMatrix = {
+			{19, 20, 28, 2,  6},
+			{21, 9, 18, -7, 24 },
+			{25, 16, 5, -7, -8},
+			{30, -13, 1, -15, 2},
+			{31, 9, -3 , -9, -18},
+		};
+
+
+		return newMatrix;
+		throw new NotImplementedException();
+	}
+}
