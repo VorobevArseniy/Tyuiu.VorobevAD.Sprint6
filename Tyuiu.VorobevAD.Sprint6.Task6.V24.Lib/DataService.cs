b387d53c12c7032f644c43d0ecffc33032ddacdc
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-26 00:16:50 +0500

    GOIDA6

---
+++ Tyuiu.VorobevAD.Sprint6.Task6.V24.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.VorobevAD.Sprint6.Task6.V24.Lib;
+
+public class DataService : ISprint6Task6V24
+{
+	public string CollectTextFromFile(string path)
+	{
+		string res = File.ReadAllText(path);
+
+		var firstLetters = res.Split([' ', '\n', '\r', '\t'], StringSplitOptions.RemoveEmptyEntries)[0];
+		res = string.Join(" ", firstLetters);
+
+		return res;
+		throw new NotImplementedException();
+	}
+}
